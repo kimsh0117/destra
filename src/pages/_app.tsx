aff4f1bb1b89bfda74bb16ba4daa8048e61bf8e4
kimsh0117 <crea.id.1703@gmail.com>
2022-06-17 01:40:09 +0300

    Set react query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,28 @@
 import '../styles/globals.css'
+import React, { useState } from 'react'
 import type { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider, Hydrate } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+          },
+        },
+      }),
+  )
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  )
 }
 
 export default MyApp
