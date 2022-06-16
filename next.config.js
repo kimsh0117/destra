c19a4aa680b24dc12d519cad6ec2390ab8c8444f
kimsh0117 <crea.id.1703@gmail.com>
2022-06-17 00:54:17 +0300

    installed packages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,28 @@
 /** @type {import('next').NextConfig} */
+
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    API_URL: process.env.API_URL,
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    API_URL: process.env.API_URL,
+    NODE_ENV: process.env.NODE_ENV,
+    ENV_NAME: process.env.ENV_NAME,
+    NEXT_AUTH_URL: process.env.NEXTAUTH_URL,
+  },
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== "development",
+  },
+};
 
-module.exports = nextConfig
+module.exports = withBundleAnalyzer({
+  ...nextConfig,
+});
